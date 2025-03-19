1377959ed62199e0af25c2f47359a66cd0ae787b
Thao Nguyen <nguyenluthao252@gmail.com>
2025-03-19 17:12:13 +0700

    cmmit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://gianguyen1234.github.io/open-english-vn',
   output: 'static',
+  outDir: 'dist', // ✅ Ensure Astro outputs files in `dist`
   integrations: [tailwind()],
 });
-
