6af159c7bbe6140ddba18dfac09b8650d0b92924
Thao Nguyen <nguyenluthao252@gmail.com>
2025-03-19 17:08:05 +0700

    fix astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://gianguyen1234.github.io',
-  base: '/open-english-vn',
+  site: 'https://gianguyen1234.github.io/open-english-vn',
   output: 'static',
   integrations: [tailwind()],
 });
+
